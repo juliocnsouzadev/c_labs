4d6edafe310d962e4914611e73608c7e90e846d6
Julio CN Souza <juliocnsouzadev@gmail.com>
2016-09-26 18:02:26 -0300

    first commit

---
+++ EstruturaDeDados/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    struct Dados {
+        int idade;
+        float peso, altura;
+    };
+    struct Dados pessoa[50];
+
+
+
+    return 0;
+}
