73db6b89b42ad59a0a77da6656ec8a4d2659bd64
Julio CN Souza <juliocnsouzadev@gmail.com>
2016-10-26 11:29:35 -0200

    code improvements

--- forca/forca.c
+++ forca/forca.c
@@ -2,53 +2,79 @@
 #include <string.h>
 
 
-int main() {
-    char palavraSecreta[20];
-    char palavra[20];
-    sprintf(palavraSecreta,"MELANCIA");
+void header(){
+    printf("================ WELCOME! LET'S PLAY A GAME? ================\n");
+}
 
-    int acertos = 0;
-    int tentativas = 0;
-    int size = strlen(palavraSecreta);
-    int acertou = 0;
+void initialization(char word[20], char secretWord[20], int* size){
+    sprintf(secretWord,"MELANCIA");
+    (*size) = strlen(secretWord);
+}
 
+void initWithUnderlines(char word[20], int size){
     for(int i = 0; i < size; i++){
-            palavra[i] = '_';
+            word[i] = '_';
     }
+}
 
-    do {
-        tentativas++;
-        
-        char chute;
-        printf("\nInforme uma letra:\n");
-        scanf(" %c", &chute);
-        printf("\n");
+char kick(int* attempts){
+    char k;
+    printf("\nGuess a letter:\n");
+    scanf(" %c", &k);
+    printf("\n");
+    (*attempts)++;
+    return k;
+}
 
-        for(int i = 0; i < size; i++){
-            char letra = palavraSecreta[i];
-            if (letra == chute) {
-                palavra[i] = letra;
-                acertos++;
-                acertou = 1;
-            }
+void guess(int size, int* rights, int* rightAttempt, char word[20], char secretWord[20], char kick){
+    for(int i = 0; i < size; i++){
+        char letter = secretWord[i];
+        if (letter == kick) {
+            word[i] = letter;
+            (*rights)++;
+            (*rightAttempt) = 1;
         }
-
-        for(int i = 0; i < size; i++){
-            char letra = palavra[i];
-            printf("%c\t", letra);
-        }
-
-        if (acertou) {
-            acertou = 0;
-        }
-        
     }
-    while (acertos != size && tentativas != size);
+}
 
-    if (acertos == size){
-        printf("\n:D Voce acertou a palavra: %s", palavraSecreta);
+void showWord(char word[20], int size){
+    for(int i = 0; i < size; i++){
+        char letter = word[i];
+        printf("%c\t", letter);
+    }
+    printf("\n");
+}
+
+void conclusion(int rights, int size, char secretWord[20]) {
+    if (rights == size){
+        printf("\nUHUUUU!!! :D\n");
+        printf("YOU WIN! THE WORD REALLY IS : %s", secretWord);
     }else{
-        printf("\n:( Voce perdeu! A palavra era: %s", palavraSecreta);
+        printf("\nOOOOPS! =/\n");
+        printf("YOU LOOSE! THE RIGHT WORD WAS: %s", secretWord);
     }
+    char end;
+    scanf(" %c", &end);
+}
+
+int main() {
+    //declarations
+    int rights =0 , attempts= 0, size = 0, rightAttempt = 0;
+    char secretWord[20];
+    char word[20];
+    //initializations
+    initialization(word, secretWord, &size);
+    initWithUnderlines(word, size);
+    //start game
+    header();
+    do {   
+        guess(size, &rights, &rightAttempt, word, secretWord,  kick(&attempts));
+        showWord(word, size);
+        if (rightAttempt) {
+            rightAttempt = 0;
+        }
+    }
+    while (rights != size && attempts != size);
+    conclusion(rights, size, secretWord);
 
 }
