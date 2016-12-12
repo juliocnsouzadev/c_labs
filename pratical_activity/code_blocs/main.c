60061ffc79ea8ebb0a62afa6e64f44be562b1a08
Julio CN Souza <juliocnsouzadev@gmail.com>
2016-12-12 15:23:33 -0200

    update

--- pratical_activity/code_blocs/main.c
+++ pratical_activity/code_blocs/main.c
@@ -1,90 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#include<stdio.h>
-#include<stdlib.h>
+int main(){
+    int vector [][3] = {{8,2,4},{7,5,2}};
 
-int a[3][3], b[3][3], c[3][3];
-int line, columms;
-
-void buildMatrizA(){
-    for (line = 0; line < 3; line++){
-        for (columms = 0; columms < 3; columms++){
-        printf("Elemento [%d][%d]: ", line + 1, columms + 1);
-        scanf("%d", &a[line][columms]);
-        }
-    }
-}
-
-void showMatrizA(){
-    for (line = 0; line < 3; line++){
-        for (columms = 0; columms < 3; columms++){
-            printf("%3d", a[line][columms]);
-        }
+    for (int i = 0; i < 3; i++) {
         printf("\n");
-    }
-}
-
-void buildMatrizB(){
-    for (line = 0; line < 3; line++){
-        for (columms = 0; columms < 3; columms++){
-        printf("Elemento [%d][%d]: ", line + 1, columms + 1);
-        scanf("%d", &b[line][columms]);
+        for (int j =0; j < 3; j++){
+            printf("\t%d",vector[i][j]);
         }
     }
-}
-
-void showMatrizB(){
-    for (line = 0; line < 3; line++){
-        for (columms = 0; columms < 3; columms++){
-            printf("%3d", b[line][columms]);
-        }
-        printf("\n");
-    }
-}
-
-void buildMatrizC(){
-    for (line = 0; line < 3; line++){
-        c[line][columms] = 0;
-        for (columms = 0; columms < 3; columms++){
-            c[line][columms] = ((a[line][columms]) * (b[line][columms]));
-        }
-    }
-}
-
-void showMatrizC(){
-    printf("\n\n");
-    printf("---------------------------- Matriz C ---------------------------------\n\n");
-    for (line = 0; line < 3; line++){
-        for (columms = 0; columms < 3; columms++){
-            printf("%3d", c[line][columms]);
-        }
-        printf("\n\n");
-    }
-}
-
-void main() {
-    buildMatrizA();
-    showMatrizA();
-
-    buildMatrizB();
-    showMatrizB();
-
-    buildMatrizC();
-    showMatrizC();
-
-    printf("\n\n");
-    printf("----------------------------Diagonal principal--------------------------------\n");
-    for (int line = 0; line <3; line++) {
-        for (int columms = 0; columms <3; columms++){
-            if (line == columms){
-                printf(" %3d", c[line][columms]);
-            }
-        }
-    }
-
-    printf("\n\n");
-
-    system("PAUSE");
-
 }
